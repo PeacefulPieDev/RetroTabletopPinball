7cf7a0351688387da66d02cdb82cc912c7edf91c
Nikita <general.governator@gmail.com>
2026-04-29 21:22:02 +0300

    Draft implementation of skip turn checkpoint

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -6,5 +6,5 @@ var shootDir = Vector3(-1, 0, 0 )
 func _integrate_forces(state):	
 	sleeping = false
 	if Input.is_key_pressed(KEY_SPACE):
-		state.apply_central_impulse(shootDir)		
+		state.apply_central_impulse(shootDir)
  
--- Scripts/Marker.gd
+++ Scripts/Marker.gd
@@ -12,4 +12,10 @@ func _on_hole_triggered(points: int):
 	var tween = create_tween()
 	for i in range(currentCheckpoint, currentCheckpoint + points):
 		tween.tween_property(self, "position", route.getCheckpointByIndex(i).position, 0.4)
+	tween.tween_callback(_on_movement_finished)
 	currentCheckpoint += points
+
+func _on_movement_finished():
+	var checkpoint = route.getCheckpointByIndex(currentCheckpoint - 1)
+	if checkpoint.skip_turn:
+		pass # Add skip turn logic here
